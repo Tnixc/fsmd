7fc52d92f579f3c3786c279f1fae1f93080d60a0
Tnixc <tnixxc@gmail.com>
2023-11-30 16:07:15 -0500

    Update app.vue and nuxt.config.ts

--- app.vue
+++ app.vue
@@ -1,12 +1,11 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <div class="h-screen fixed inset-0 bg-black">
+  <div class="overflow-hidden">
+    <div class="h-screen fixed inset-0 bg-black -z-50">
       <div
         class="lg:w-1/2 h-screen bg-gradient-to-b from-zinc-700 overflow-hidden"
       >
-        <div id="int" class="h-full bg-zinc-950">
-          <div class="text-gray-500 pl-24">
+        <div id="int" class="h-full bg-zinc-950 fixed left-0 top-0 right-1/2">
+          <div class="text-gray-500 lg:pl-24 p-5">
             <h1
               id="h"
               class="relative tracking-widest text-9xl mt-48 block bg-gradient-to-b from-zinc-50 to-slate-400 inline-block text-transparent bg-clip-text"
@@ -27,6 +26,9 @@
           </div>
         </div>
       </div>
+    </div>
+    <div class="lg:w-1/2 lg:fixed right-10 top-1/2 -translate-y-1/2 -z-30" id="x">
+      <img :src="svg" />
     </div>
   </div>
 </template>
@@ -57,7 +59,7 @@
   right: -100%;
   z-index: 1;
   background: repeating-linear-gradient(
-    50deg,
+    45deg,
     #00000020,
     #00000020 1px,
     transparent 1px,
@@ -73,17 +75,49 @@
   right: -100%;
   background: linear-gradient(
     73deg,
-    rgba(13, 82, 222, 1) 0%,
-    rgba(10, 209, 186, 1) 19%,
-    rgba(33, 193, 235, 1) 56%,
-    rgba(196, 68, 255, 1) 76%,
-    rgba(234, 197, 229, 1) 92%
+    rgba(13, 82, 222, 0.6) 0%,
+    rgba(10, 209, 186, 0.3) 25%,
+    rgba(33, 193, 235, 0.6) 50%,
+    rgba(196, 68, 255, 0.3) 75%,
+    rgba(234, 197, 229, 0.6) 100%
   );
   opacity: 1;
   z-index: 0;
 }
-#sep body {
+body {
   font-family: Geist, sans-serif;
   font-size: 16px;
 }
+#x:after{
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: linear-gradient(180deg, #D7D3D2, transparent);
+  z-index: 998;
+}
+#x:before{
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: repeating-linear-gradient(
+    45deg,
+    #00000088,
+    #00000055 1px,
+    transparent 1px,
+    transparent 6px
+  );
+  z-index: 999;
+}
 </style>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
