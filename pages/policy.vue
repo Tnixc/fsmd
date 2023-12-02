212e07f1773d11a261695fcf114e9efaa8ffcc82
Tnixc <tnixxc@gmail.com>
2023-12-01 19:50:01 -0500

    Add marked library for markdown parsing

--- pages/policy.vue
+++ pages/policy.vue
@@ -1,3 +1,26 @@
+<script>
+import { marked } from "marked";
+
+export default {
+  data() {
+    return {
+      policy: "",
+    };
+  },
+  mounted() {
+    fetch(
+      "https://raw.githubusercontent.com/nodeca/babelfish/master/CHANGELOG.md"
+    )
+      .then((res) => res.text())
+      .then((res) => {
+        this.policy = marked.parse(res);
+      });
+  },
+};
+</script>
+
 <template>
-  <h1>Hello</h1>
+  <div class="text-white">
+    <div v-html="policy"></div>
+  </div>
 </template>
